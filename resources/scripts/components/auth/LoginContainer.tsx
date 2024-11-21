7e08efe56851420902cb4aed8c82e3635658042d
Synth Hosting <168385588+synthhosting@users.noreply.github.com>
2024-11-21 14:45:28 -0500

    Fix

--- resources/scripts/components/auth/LoginContainer.tsx
+++ resources/scripts/components/auth/LoginContainer.tsx
@@ -28,10 +28,6 @@ const LoginContainer = ({ history }: RouteComponentProps) => {
 
     const { clearFlashes, clearAndAddHttpError } = useFlash();
     const { enabled: recaptchaEnabled, siteKey } = useStoreState((state) => state.settings.data!.recaptcha);
-    const [passwordShown, setPasswordShown] = useState(false);
-    const togglePassword = () => {
-        setPasswordShown(!passwordShown);
-    };
 
     useEffect(() => {
         clearFlashes();
@@ -40,6 +36,8 @@ const LoginContainer = ({ history }: RouteComponentProps) => {
     const onSubmit = (values: Values, { setSubmitting }: FormikHelpers<Values>) => {
         clearFlashes();
 
+        // If there is no token in the state yet, request the token and then abort this submit request
+        // since it will be re-submitted when the recaptcha data is returned by the component.
         if (recaptchaEnabled && !token) {
             ref.current!.execute().catch((error) => {
                 console.error(error);
@@ -85,10 +83,7 @@ const LoginContainer = ({ history }: RouteComponentProps) => {
                 <LoginFormContainer title={'Login to Continue'} css={tw`w-full flex`}>
                     <Field type={'text'} label={'Username or Email'} name={'username'} disabled={isSubmitting} />
                     <div css={tw`mt-6 flex justify-between items-center`}>
-                        <Field light type={passwordShown ? "text" : "password"} label={'Password'} name={'password'} disabled={isSubmitting} />
-                        <button type="button" onClick={togglePassword} css={tw`text-xs uppercase mb-1 sm:mb-2`}>
-                            {passwordShown ? 'Hide' : 'Show'} Password
-                        </button>
+                        <label htmlFor="password" css={tw`text-xs uppercase mb-1 sm:mb-2`}>Password</label>
                         <Link
                             to={'/auth/password'}
                             css={tw`text-xs tracking-wide no-underline uppercase hover:opacity-80 mb-1 sm:mb-2`}
@@ -96,12 +91,13 @@ const LoginContainer = ({ history }: RouteComponentProps) => {
                             Forgot password?
                         </Link>
                     </div>
+                    <Field type={'password'} name={'password'} disabled={isSubmitting} />
                     <div css={tw`mt-6`}>
                         <Button
-                            css={tw`w-full my-2 overflow-hidden whitespace-nowrap`}
-                            type={"submit"}
-                            disabled={isSubmitting}
-                        >
+                                css={tw`w-full my-2 overflow-hidden whitespace-nowrap`}
+                                type={"submit"}
+                                disabled={isSubmitting}
+                            >
                             Login
                         </Button>
                     </div>
@@ -123,13 +119,13 @@ const LoginContainer = ({ history }: RouteComponentProps) => {
                     {(auth_google_status == true || auth_discord_status == true || auth_github_status == true) &&
                         <div css={tw`mt-2 text-center`}>
                             {auth_google_status == true &&
-                                <a href={'/auth/login/google'}>
+                            <a href={'/auth/login/google'}>
                                     <i className="bi bi-google text-xl p-2 mx-2 rounded-full bg-[#4e8df5]"></i>
                                 </a>
                             }
                             {auth_discord_status == true &&
                                 <a href={'/auth/login/discord'}>
-                                    <i className="bi bi-discord text-xl p-2 mx-2 rounded-full bg-[#5865F2]"></i>
+                                        <i className="bi bi-discord text-xl p-2 mx-2 rounded-full bg-[#5865F2]"></i>
                                 </a>
                             }
                             {auth_github_status == true &&
