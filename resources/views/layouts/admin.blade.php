eaaec9d3e6c1f437e03209adcb8f016509c06b7e
Synth Hosting <168385588+synthhosting@users.noreply.github.com>
2024-11-26 17:37:49 -0500

    test

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -70,7 +70,7 @@
         (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/662bb2981ec1082f04e77811/1hsdb39bb';
+        s1.src='{{ config('app.tawkto', 'notprovided') }}';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
