ec98705cf5a0be10bb32afa07fecf8274934b756
Synth Hosting <168385588+synthhosting@users.noreply.github.com>
2024-11-29 21:14:24 -0500

    fix

--- resources/scripts/components/UserAvatar.tsx
+++ resources/scripts/components/UserAvatar.tsx
@@ -1,34 +1,22 @@
 import * as React from 'react';
 import { useStoreState } from 'easy-peasy';
 import { ApplicationStore } from '@/state';
-import Avatar from '@/components/Avatar';
 
 interface Props {
     email?: string;
-    user?: string;
-    uuid?: string;
     width?: string;
     rounded?: string;
 }
 
-export default ({ email, user, uuid, width, rounded }: Props) => {
-    const profileType = useStoreState((state: ApplicationStore) => state.settings.data?.profileType);
-    const username = useStoreState((state) => state.user.data?.username);
-    const useremail = useStoreState(state => state.user.data?.email);
+export default ({ email, width, rounded }: Props) => {
+    const useremail = useStoreState((state: ApplicationStore) => state.user.data?.email);
 
     return (
-        profileType === 'boring'
-        ? <div className={`${rounded ? rounded : 'rounded-full'} overflow-hidden flex items-center`} css={`width:${width ? width : '32px'};`}>{uuid ? <Avatar name={uuid} /> : <Avatar.User />}</div>
-        : profileType === 'gravatar'
-        ? <img src={`https://www.gravatar.com/avatar/${email ? email : useremail}`} width={width ? width : '32px'} className={rounded ? rounded : 'rounded-full'} alt='Gravatar' />
-        : profileType === 'avataaars'
-        ? <img src={`https://api.dicebear.com/7.x/big-ears-neutral/svg?seed=${user ? user : username}`} width={width ? width : '32px'} className={rounded ? rounded : 'rounded-full'} alt='Dicebear Avatar' />
-        : profileType === 'bottts'
-        ? <img src={`https://api.dicebear.com/7.x/bottts-neutral/svg?seed=${user ? user : username}`} width={width ? width : '32px'} className={rounded ? rounded : 'rounded-full'} alt='Dicebear Avatar' />
-        : profileType === 'identicon'
-        ? <img src={`https://api.dicebear.com/7.x/identicon/svg?seed=${user ? user : username}`} width={width ? width : '32px'} className={rounded ? rounded : 'rounded-full'} alt='Dicebear Avatar' />
-        : profileType === 'initials'
-        ? <img src={`https://api.dicebear.com/7.x/initials/svg?seed=${user ? user : username}`} width={width ? width : '32px'} className={rounded ? rounded : 'rounded-full'} alt='Dicebear Avatar' />
-        : <></>
+        <img
+            src={`https://www.gravatar.com/avatar/${email ? email : useremail}`}
+            width={width ? width : '32px'}
+            className={rounded ? rounded : 'rounded-full'}
+            alt='Gravatar'
+        />
     );
 };
